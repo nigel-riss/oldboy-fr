8113e5f21772d0b29af6da0f71af3fdba4210c87
nigel-riss <mr.kurenkov@gmail.com>
2017-07-14 14:20:13 +0200

    Basic workflow finished

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    watch('./src/**/*.pug', () => {
+        gulp.start('pugChanged');
+    })
+
+    watch('./src/**/*.scss', () => {
+        gulp.start('cssInject');
+    })
+});
+
+gulp.task('pugChanged', ['pugRender'], () => {
+    browserSync.reload();
+});
+
+gulp.task('cssInject', ['styles'], () => {
+    gulp.src('./dist/styles.css')
+    .pipe(browserSync.stream());
+});
